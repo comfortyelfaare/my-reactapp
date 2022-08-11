a6d91583d6bd5c5c523ca918d3f83f19558898c6
comfortyelfaare <110114807+comfortyelfaare@users.noreply.github.com>
2022-08-11 10:14:05 -0700

    comment 1

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+    <h1>wlcome to my shpe</h1>
+    
+    <nav>
+<div>
+<ul>
+<li>home</li>
+<li>about</li>
+<li>products</li>
+<li>contact</li>
+
+</ul>
+</div>
+    </nav>
+    </div>
+    ) 
+}
+
+export default NavBar
